0ce492126359e5d1a57a8290f42711430ce8fa9a
Emilio Spatola <emiliosp.me@gmail.com>
2026-09-21 17:52:27 +0200

    chore: improvements

---
+++ src/utils/path-exists.ts
@@ -0,0 +1,13 @@
+import { access } from 'node:fs/promises';
+
+export const pathExists = async (path: string): Promise<boolean> => {
+  try {
+    await access(path);
+    return true;
+  } catch (error) {
+    if ((error as NodeJS.ErrnoException).code === 'ENOENT') {
+      return false;
+    }
+    throw error;
+  }
+};
